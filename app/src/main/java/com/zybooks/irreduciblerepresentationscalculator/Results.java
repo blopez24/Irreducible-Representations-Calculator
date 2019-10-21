d0ac3f1c89b4b737a06018d34b1ce7bea7520b38
jrodt333 <jrodt333@gmail.com>
2019-10-21 13:40:11 -0700

    made results.java, uses results.xml for view

---
+++ app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/Results.java
@@ -0,0 +1,14 @@
+package com.zybooks.irreduciblerepresentationscalculator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Results extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.results);
+    }
+}
