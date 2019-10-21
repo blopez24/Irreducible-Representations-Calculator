d7b01f37950157f97e2607593095d77edede39cf
Hsiang-Li Chung <hchung14@ucsc.edu>
2019-10-21 14:13:44 -0700

    added second page not really sure how to do the table and the clickable link, i'll try to look for more info added edit text to the table

--- app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/Results.java
+++ app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/Results.java
@@ -1,14 +1,24 @@
 package com.zybooks.irreduciblerepresentationscalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Results extends AppCompatActivity {
 
+    TextView text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results);
+
+        text = findViewById(R.id.cell);
+        Intent it = getIntent();
+        Bundle bundle = it.getExtras();
+        text.setText(bundle.get("value").toString());
+
     }
 }
