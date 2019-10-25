9ac9fb32897977f69355a0be22011ff4eab7e968
jrodt333 <jrodt333@gmail.com>
2019-10-25 14:46:29 -0700

    began the calculation part of Calculation.java

--- app/src/androidTest/java/com/zybooks/irreduciblerepresentationscalculator/Calculation.java
+++ app/src/androidTest/java/com/zybooks/irreduciblerepresentationscalculator/Calculation.java
@@ -2,19 +2,20 @@ package com.zybooks.irreduciblerepresentationscalculator;
 
 public class Calculation {
     private String element;
-    private int [] args;
     private Object[][] charTable;
+    private String result;
 
-    Calculation(String element, int[] args){
+    Calculation(String element){
         this.element = element;
-        this.args = args; // user input
+        result = ""; // will be something like "2A1 + B2" eventually
         switch (element){
             case "c2v":
                 // c2v's charTable, first row is coefficients of E, C2, etc
                 // other rows are the values in the table (A1's are +1, +1, +1, +1)
                 // is a 2d object array because row names vary by element so have to store as String
+                // charTable [0][0] stores the value used for h in the equation
                 charTable = new Object[5][5];
-                charTable[0][0] = "C2V";
+                charTable[0][0] = 4;
                 charTable[0][1] = 1;
                 charTable[0][2] = 1;
                 charTable[0][3] = 1;
@@ -46,8 +47,18 @@ public class Calculation {
         }
     }
 
-    public String result(){
-        return "Something like 2A1 + B2 eventually";
+    public void calculate(int[] input){
+        // input is an array because the amount of args varies by element
+        int h;
+        if(charTable[0][0] instanceof Integer)
+            h = (Integer) charTable[0][0];
+        
+
+
+    }
+
+    public String getResult(){
+        return result;
     }
 
 
