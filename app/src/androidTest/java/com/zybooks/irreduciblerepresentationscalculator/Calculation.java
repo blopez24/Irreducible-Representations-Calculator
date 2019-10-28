3e60b610aa594609e4bd28217fddd82231f529b9
Bryan <blopez24@ucsc.edu>
2019-10-27 23:45:36 -0700

    move a bit calculations

--- app/src/androidTest/java/com/zybooks/irreduciblerepresentationscalculator/Calculation.java
+++ app/src/androidTest/java/com/zybooks/irreduciblerepresentationscalculator/Calculation.java
@@ -15,31 +15,11 @@ public class Calculation {
                 // is a 2d object array because row names vary by element so have to store as String
                 // charTable [0][0] stores the value used for h in the equation
                 charTable = new Object[5][5];
-                charTable[0][0] = 4;
-                charTable[0][1] = 1;
-                charTable[0][2] = 1;
-                charTable[0][3] = 1;
-                charTable[0][4] = 1;
-                charTable[1][0] = "A1";
-                charTable[1][1] = 1;
-                charTable[1][2] = 1;
-                charTable[1][3] = 1;
-                charTable[1][4] = 1;
-                charTable[2][0] = "A2";
-                charTable[2][1] = 1;
-                charTable[2][2] = 1;
-                charTable[2][3] = -1;
-                charTable[2][4] = -1;
-                charTable[3][0] = "A1";
-                charTable[3][1] = 1;
-                charTable[3][2] = -1;
-                charTable[3][3] = 1;
-                charTable[3][4] = -1;
-                charTable[4][0] = "A1";
-                charTable[4][1] = 1;
-                charTable[4][2] = -1;
-                charTable[4][3] = -1;
-                charTable[4][4] = 1;
+                charTable[0][0] = 4;    charTable[0][1] = 1; charTable[0][2] = 1; charTable[0][3] = 1; charTable[0][4] = 1;
+                charTable[1][0] = "A1"; charTable[1][1] = 1; charTable[1][2] = 1; charTable[1][3] = 1; charTable[1][4] = 1;
+                charTable[2][0] = "A2"; charTable[2][1] = 1; charTable[2][2] = 1; charTable[2][3] = -1; charTable[2][4] = -1;
+                charTable[3][0] = "B1"; charTable[3][1] = 1; charTable[3][2] = -1; charTable[3][3] = 1; charTable[3][4] = -1;
+                charTable[4][0] = "B2"; charTable[4][1] = 1; charTable[4][2] = -1; charTable[4][3] = -1; charTable[4][4] = 1;
                 break;
             default:
                 charTable = null;
