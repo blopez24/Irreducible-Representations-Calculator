4812e0ada3daf7db7773bc0fe1a8291267735bbe
jrodt333 <jrodt333@gmail.com>
2019-11-01 01:06:56 -0700

    completed some of the tables in TableData.java

--- app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/TableData.java
+++ app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/TableData.java
@@ -53,26 +53,6 @@ public class TableData
                 charTable[2][0] = "B"; charTable[2][1] = 1; charTable[2][2] = -1;
                 break;
 
-            case "c3":
-                // Number of symmetry elements  h = 3
-                // Ask Professor
-                break;
-
-            case "c4":
-                // Number of symmetry elements  h = 4
-                // Ask Professor
-                break;
-
-            case "c5":
-                // Number of symmetry elements  h = 5
-                // Ask Professor
-                break;
-
-            case "c6":
-                // Number of symmetry elements  h = 6
-                // Ask Professor
-                break;
-
             case "c2v":
                 // c2v's charTable, first row is coefficients of E, C2, etc
                 // other rows are the values in the table (A1's are +1, +1, +1, +1)
@@ -115,22 +95,6 @@ public class TableData
                 charTable[5][0] = "E";  charTable[5][1] = 2; charTable[5][2] = 0; charTable[5][3] = -2; charTable[5][4] = 0; charTable[5][5] = 0;
                 break;
 
-            // shouldn't need the following because we don't have to do point groups w/cosine, but leaving in comments because it's already done and in case things change
-            /*case "c5v":
-                // Number of symmetry elements	h = 10
-                // C5v	E	2C5 (z)	2(C5)2	5v
-                // A1	+1	+1	+1	+1
-                // A2	+1	+1	+1	-1
-                // E1	+2	+2cos(2/5)	+2cos(4/5)	0
-                // E2	+2	+2cos(4/5)	+2cos(2/5)	0
-                charTable = new Object[5][5];
-                charTable[0][0] = 10;   charTable[0][1] = 1; charTable[0][2] = 2; charTable[0][3] = 2; charTable[0][4] = 5;
-                charTable[1][0] = "A1"; charTable[1][1] = 1; charTable[1][2] = 1; charTable[1][3] = 1; charTable[1][4] = 1;
-                charTable[2][0] = "A2"; charTable[2][1] = 1; charTable[2][2] = 1; charTable[2][3] = 1; charTable[2][4] = -1;
-                charTable[3][0] = "E1"; charTable[3][1] = 2; charTable[3][2] = 2*Math.cos(2*Math.pi/5); charTable[3][3] = 2*Math.cos(4*Math.pi/5); charTable[3][4] = 0;
-                charTable[4][0] = "E2"; charTable[4][1] = 2; charTable[4][2] = 2*Math.cos(4*Math.pi/5); charTable[4][3] = 2*Math.cos(2*Math.pi/5); charTable[4][4] = 0;
-                break;*/
-
             case "c6v":
                 // Number of symmetry elements	h = 12
                 // C6v	E	2C6 (z)	2C3 (z)	C2 (z)	3v	3d
@@ -147,7 +111,7 @@ public class TableData
                 charTable[3][0] = "B1"; charTable[3][1] = 1; charTable[3][2] = -1; charTable[3][3] = 1; charTable[3][4] = -1; charTable[3][5] = 1; charTable[3][6] = -1;
                 charTable[4][0] = "B2"; charTable[4][1] = 1; charTable[4][2] = -1; charTable[4][3] = 1; charTable[4][4] = -1; charTable[4][5] = -1; charTable[4][6] = 1;
                 charTable[5][0] = "E1"; charTable[5][1] = 2; charTable[5][2] = -1; charTable[5][3] = -1; charTable[5][4] = 2; charTable[5][5] = 0; charTable[5][6] = 0;
-                charTable[6][0] = "E2"; charTable[5][1] = 2; charTable[5][2] = -1; charTable[5][3] = -1; charTable[5][4] = 2; charTable[5][5] = 0; charTable[5][6] = 0;
+                charTable[6][0] = "E2"; charTable[6][1] = 2; charTable[6][2] = -1; charTable[6][3] = -1; charTable[6][4] = 2; charTable[6][5] = 0; charTable[6][6] = 0;
                 break;
 
             case "c2h":
@@ -163,18 +127,6 @@ public class TableData
                 charTable[2][0] = "Bg"; charTable[2][1] = 1; charTable[2][2] = -1; charTable[2][3] = 1; charTable[2][4] = -1;
                 charTable[3][0] = "Au"; charTable[3][1] = 1; charTable[3][2] = 1; charTable[3][3] = -1; charTable[3][4] = -1;
                 charTable[4][0] = "Bu"; charTable[4][1] = 1; charTable[4][2] = -1; charTable[4][3] = -1; charTable[4][4] = 1;
-                break;
-
-            case "c3h":
-                break;
-
-            case "c4h":
-                break;
-
-            case "c5h":
-                break;
-
-            case "c6h":
                 break;
 
             case "d2":
@@ -238,7 +190,7 @@ public class TableData
                 charTable[3][0] = "B1"; charTable[3][1] = 1; charTable[3][2] = -1; charTable[3][3] = 1; charTable[3][4] = -1; charTable[3][5] = 1; charTable[3][6] = -1;
                 charTable[4][0] = "B2"; charTable[4][1] = 1; charTable[4][2] = -1; charTable[4][3] = 1; charTable[4][4] = -1; charTable[4][5] = -1; charTable[4][6] = 1;
                 charTable[5][0] = "E1"; charTable[5][1] = 2; charTable[5][2] = 1; charTable[5][3] = -1; charTable[5][4] = -2; charTable[5][5] = 0; charTable[5][6] = 0;
-                charTable[6][0] = "E2"; charTable[5][1] = 2; charTable[5][2] = -1; charTable[5][3] = -1; charTable[5][4] = 2; charTable[5][5] = 0; charTable[5][6] = 0;
+                charTable[6][0] = "E2"; charTable[6][1] = 2; charTable[6][2] = -1; charTable[6][3] = -1; charTable[6][4] = 2; charTable[6][5] = 0; charTable[6][6] = 0;
                 break;
 
             case "d2d":
@@ -274,31 +226,83 @@ public class TableData
                 charTable[3][0] = "Eg";  charTable[3][1] = 2; charTable[3][2] = -1; charTable[3][3] = 0; charTable[3][4] = 2; charTable[3][5] = -1; charTable[3][6] = 0;
                 charTable[4][0] = "A1u"; charTable[4][1] = 1; charTable[4][2] = 1; charTable[4][3] = 1; charTable[4][4] = -1; charTable[4][5] = -1; charTable[4][6] = -1;
                 charTable[5][0] = "A2u"; charTable[5][1] = 1; charTable[5][2] = 1; charTable[5][3] = -1; charTable[5][4] = -1; charTable[5][5] = -1; charTable[5][6] = 1;
-                charTable[6][0] = "Eu";  charTable[5][1] = 2; charTable[5][2] = -1; charTable[5][3] = 0; charTable[5][4] = -2; charTable[5][5] = 1; charTable[5][6] = 0;
+                charTable[6][0] = "Eu";  charTable[6][1] = 2; charTable[6][2] = -1; charTable[6][3] = 0; charTable[6][4] = -2; charTable[6][5] = 1; charTable[6][6] = 0;
                 break;
 
             case "d4d":
-                break;
-
-            case "d5d":
+                // Number of symmetry elements	h = 16
+                // D4d	E	2S8	2C4	2(S8)3	C2	4C'2	4d
+                // A1	+1	+1	+1	+1	+1	+1	+1
+                // A2	+1	+1	+1	+1	+1	-1	-1
+                // B1	+1	-1	+1	-1	+1	+1	-1
+                // B2	+1	-1	+1	-1	+1	-1	+1
+                // E1	+2	+(2)½	0	-(2)½	-2	0	0
+                // E2	+2	0	-2	0	+2	0	0
+                // E3	+2	-(2)½	0	+(2)½	-2	0	0
+                charTable = new Object[8][8];
+                charTable[0][0] = 16;   charTable[0][1] = 1; charTable[0][2] = 2; charTable[0][3] = 2; charTable[0][4] = 2; charTable[0][5] = 1; charTable[0][6] = 4; charTable[0][7] = 4;
+                charTable[1][0] = "A1"; charTable[1][1] = 1; charTable[1][2] = 1; charTable[1][3] = 1; charTable[1][4] = 1; charTable[1][5] = 1; charTable[1][6] = 1; charTable[1][7] = 1;
+                charTable[2][0] = "A2"; charTable[2][1] = 1; charTable[2][2] = 1; charTable[2][3] = 1; charTable[2][4] = 1; charTable[2][5] = 1; charTable[2][6] = -1; charTable[2][7] = -1;
+                charTable[3][0] = "B1"; charTable[3][1] = 1; charTable[3][2] = -1; charTable[3][3] = 1; charTable[3][4] = -1; charTable[3][5] = 1; charTable[3][6] = 1; charTable[3][7] = -1;
+                charTable[4][0] = "B2"; charTable[4][1] = 1; charTable[4][2] = -1; charTable[4][3] = 1; charTable[4][4] = -1; charTable[4][5] = 1; charTable[4][6] = -1; charTable[4][7] = 1;
+                charTable[5][0] = "E1"; charTable[5][1] = 2; charTable[5][2] = Math.pow(2,0.5); charTable[5][3] = 0; charTable[5][4] = -1*Math.pow(2,0.5); charTable[5][5] = -2; charTable[5][6] = 0; charTable[5][7] = 0;
+                charTable[6][0] = "E2"; charTable[6][1] = 2; charTable[6][2] = 0; charTable[6][3] = -2; charTable[6][4] = 0; charTable[6][5] = 2; charTable[6][6] = 0; charTable[6][7] = 0;
+                charTable[7][0] = "E3"; charTable[7][1] = 2; charTable[7][2] = -1*Math.pow(2,0.5); charTable[7][3] = 0; charTable[7][4] = Math.pow(2,0.5); charTable[7][5] = -2; charTable[7][6] = 0; charTable[7][7] = 0;
                 break;
 
             case "d6d":
+                // To-Do: 10 rows
                 break;
 
             case "d2h":
+                // Number of symmetry elements	h = 8
+                // D2h	E	C2 (z)	C2 (y)	C2 (x)	i	 (xy)	 (xz)	 (yz)
+                // Ag	+1	+1	+1	+1	+1	+1	+1	+1
+                // B1g	+1	+1	-1	-1	+1	+1	-1	-1
+                // B2g	+1	-1	+1	-1	+1	-1	+1	-1
+                // B3g	+1	-1	-1	+1	+1	-1	-1	+1
+                // Au	+1	+1	+1	+1	-1	-1	-1	-1
+                // B1u	+1	+1	-1	-1	-1	-1	+1	+1
+                // B2u	+1	-1	+1	-1	-1	+1	-1	+1
+                // B3u	+1	-1	-1	+1	-1	+1	+1	-1
+                charTable = new Object[9][9];
+                charTable[0][0] = 8;     charTable[0][1] = 1; charTable[0][2] = 1; charTable[0][3] = 1; charTable[0][4] = 1; charTable[0][5] = 1; charTable[0][6] = 1; charTable[0][7] = 1; charTable[0][8] = 1;
+                charTable[1][0] = "Ag";  charTable[1][1] = 1; charTable[1][2] = 1; charTable[1][3] = 1; charTable[1][4] = 1; charTable[1][5] = 1; charTable[1][6] = 1; charTable[1][7] = 1; charTable[1][8] = 1;
+                charTable[2][0] = "B1g"; charTable[2][1] = 1; charTable[2][2] = 1; charTable[2][3] = -1; charTable[2][4] = -1; charTable[2][5] = 1; charTable[2][6] = 1; charTable[2][7] = -1; charTable[2][8] = -1;
+                charTable[3][0] = "B2g"; charTable[3][1] = 1; charTable[3][2] = -1; charTable[3][3] = 1; charTable[3][4] = -1; charTable[3][5] = 1; charTable[3][6] = -1; charTable[3][7] = 1; charTable[3][8] = -1;
+                charTable[4][0] = "B3g"; charTable[4][1] = 1; charTable[4][2] = -1; charTable[4][3] = -1; charTable[4][4] = 1; charTable[4][5] = 1; charTable[4][6] = -1; charTable[4][7] = -1; charTable[4][8] = 1;
+                charTable[5][0] = "Au";  charTable[5][1] = 1; charTable[5][2] = 1; charTable[5][3] = 1; charTable[5][4] = 1; charTable[5][5] = -1; charTable[5][6] = -1; charTable[5][7] = -1; charTable[5][8] = -1;
+                charTable[6][0] = "Blu"; charTable[6][1] = 1; charTable[6][2] = 1; charTable[6][3] = -1; charTable[6][4] = -1; charTable[6][5] = -1; charTable[6][6] = -1; charTable[6][7] = 1; charTable[6][8] = 1;
+                charTable[7][0] = "B2u"; charTable[7][1] = 1; charTable[7][2] = -1; charTable[7][3] = 1; charTable[7][4] = -1; charTable[7][5] = -1; charTable[7][6] = 1; charTable[7][7] = -1; charTable[7][7] = 1;
+                charTable[8][0] = "B3u"; charTable[8][1] = 1; charTable[8][2] = -1; charTable[8][3] = -1; charTable[8][4] = 1; charTable[8][5] = -1; charTable[8][6] = 1; charTable[8][7] = 1; charTable[8][7] = -1;
                 break;
 
             case "d3h":
+                // Number of symmetry elements	h = 12
+                // D3h	E	2C3 (z)	3C'2	h (xy)	2S3	3v
+                // A'1	+1	+1	+1	+1	+1	+1
+                // A'2	+1	+1	-1	+1	+1	-1
+                // E'	+2	-1	0	+2	-1	0
+                // A''1	+1	+1	+1	-1	-1	-1
+                // A''2	+1	+1	-1	-1	-1	+1
+                // E''	+2	-1	0	-2	+1	0
+                charTable = new Object[7][7];
+                charTable[0][0] = 12;     charTable[0][1] = 1; charTable[0][2] = 2; charTable[0][3] = 3; charTable[0][4] = 1; charTable[0][5] = 2; charTable[0][6] = 3;
+                charTable[1][0] = "A'1";  charTable[1][1] = 1; charTable[1][2] = 1; charTable[1][3] = 1; charTable[1][4] = 1; charTable[1][5] = 1; charTable[1][6] = 1;
+                charTable[2][0] = "A'2";  charTable[2][1] = 1; charTable[2][2] = 1; charTable[2][3] = -1; charTable[2][4] = 1; charTable[2][5] = 1; charTable[2][6] = -1;
+                charTable[3][0] = "E'";   charTable[3][1] = 2; charTable[3][2] = -1; charTable[3][3] = 0; charTable[3][4] = 2; charTable[3][5] = -1; charTable[3][6] = 0;
+                charTable[4][0] = "A\"1"; charTable[4][1] = 1; charTable[4][2] = 1; charTable[4][3] = 1; charTable[4][4] = -1; charTable[4][5] = -1; charTable[4][6] = -1;
+                charTable[5][0] = "A\"2"; charTable[5][1] = 1; charTable[5][2] = 1; charTable[5][3] = -1; charTable[5][4] = -1; charTable[5][5] = -1; charTable[5][6] = 1;
+                charTable[6][0] = "E\"";  charTable[6][1] = 2; charTable[6][2] = -1; charTable[6][3] = 0; charTable[6][4] = -2; charTable[6][5] = 1; charTable[6][6] = 0;
                 break;
 
             case "d4h":
+                // To-Do: 11 rows
                 break;
 
-            case "d5h":
-                break;
 
             case "d6h":
+                // To-Do: 13 rows
                 break;
 
             case "td":
@@ -308,6 +312,7 @@ public class TableData
                 break;
 
             case "oh":
+                // To-Do: 11 rows
                 break;
 
             default:
