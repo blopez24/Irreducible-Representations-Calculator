18f10c1cdd62db20bc5b31ff6c74fd910f42b1ca
jrodt333 <jrodt333@gmail.com>
2019-10-31 18:21:42 -0700

    completed some of the tables in TableData.java

--- app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/TableData.java
+++ app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/TableData.java
@@ -98,6 +98,126 @@ public class TableData
                 charTable[3][0] = "E";  charTable[3][1] = 2; charTable[3][2] = -1; charTable[3][3] = 0;
                 break;
 
+            case "c4v":
+                // Number of symmetry elements	h = 8
+                // C4v	E	2C4(z)	C2	2v	2d
+                // A1	+1	+1	+1	+1	+1
+                // A2	+1	+1	+1	-1	-1
+                // B1	+1	-1	+1	+1	-1
+                // B2	+1	-1	+1	-1	+1
+                // E	+2	0	-2	0	0
+                charTable = new Object[6][6];
+                charTable[0][0] = 8;    charTable[0][1] = 1; charTable[0][2] = 2; charTable[0][3] = 1; charTable[0][4] = 2; charTable[0][5] = 2;
+                charTable[1][0] = "A1"; charTable[1][1] = 1; charTable[1][2] = 1; charTable[1][3] = 1; charTable[1][4] = 1; charTable[1][5] = 1;
+                charTable[2][0] = "A2"; charTable[2][1] = 1; charTable[2][2] = 1; charTable[2][3] = 1; charTable[2][4] = -1; charTable[2][5] = -1;
+                charTable[3][0] = "B1"; charTable[3][1] = 1; charTable[3][2] = -1; charTable[3][3] = 1; charTable[3][4] = 1; charTable[3][5] = 1;
+                charTable[4][0] = "B2"; charTable[4][1] = 1; charTable[4][2] = -1; charTable[4][3] = 1; charTable[4][4] = -1; charTable[4][5] = 1;
+                charTable[5][0] = "E";  charTable[5][1] = 2; charTable[5][2] = 0; charTable[5][3] = -2; charTable[5][4] = 0; charTable[5][5] = 0;
+                break;
+
+            case "c5v":
+                // Number of symmetry elements	h = 10
+                // C5v	E	2C5 (z)	2(C5)2	5v
+                // A1	+1	+1	+1	+1
+                // A2	+1	+1	+1	-1
+                // E1	+2	+2cos(2/5)	+2cos(4/5)	0
+                // E2	+2	+2cos(4/5)	+2cos(2/5)	0
+                charTable = new Object[5][5];
+                charTable[0][0] = 10;   charTable[0][1] = 1; charTable[0][2] = 2; charTable[0][3] = 2; charTable[0][4] = 5;
+                charTable[1][0] = "A1"; charTable[1][1] = 1; charTable[1][2] = 1; charTable[1][3] = 1; charTable[1][4] = 1;
+                charTable[2][0] = "A2"; charTable[2][1] = 1; charTable[2][2] = 1; charTable[2][3] = 1; charTable[2][4] = -1;
+                charTable[3][0] = "E1"; charTable[3][1] = 2; charTable[3][2] = 2*Math.cos(2*Math.pi/5); charTable[3][3] = 2*Math.cos(4*Math.pi/5); charTable[3][4] = 0;
+                charTable[4][0] = "E2"; charTable[4][1] = 2; charTable[4][2] = 2*Math.cos(4*Math.pi/5); charTable[4][3] = 2*Math.cos(2*Math.pi/5); charTable[4][4] = 0;
+                break;
+
+            case "c6v":
+                // Number of symmetry elements	h = 12
+                // C6v	E	2C6 (z)	2C3 (z)	C2 (z)	3v	3d
+                // A1	+1	+1	+1	+1	+1	+1
+                // A2	+1	+1	+1	+1	-1	-1
+                // B1	+1	-1	+1	-1	+1	-1
+                // B2	+1	-1	+1	-1	-1	+1
+                // E1	+2	+1	-1	-2	0	0
+                // E2	+2	-1	-1	+2	0	0
+                charTable = new Object[7][7];
+                charTable[0][0] = 12;   charTable[0][1] = 1; charTable[0][2] = 2; charTable[0][3] = 2; charTable[0][4] = 1; charTable[0][5] = 3; charTable[0][6] = 3;
+                charTable[1][0] = "A1"; charTable[1][1] = 1; charTable[1][2] = 1; charTable[1][3] = 1; charTable[1][4] = 1; charTable[1][5] = 1; charTable[1][6] = 1;
+                charTable[2][0] = "A2"; charTable[2][1] = 1; charTable[2][2] = 1; charTable[2][3] = 1; charTable[2][4] = 1; charTable[2][5] = -1; charTable[2][6] = -1;
+                charTable[3][0] = "B1"; charTable[3][1] = 1; charTable[3][2] = -1; charTable[3][3] = 1; charTable[3][4] = -1; charTable[3][5] = 1; charTable[3][6] = -1;
+                charTable[4][0] = "B2"; charTable[4][1] = 1; charTable[4][2] = -1; charTable[4][3] = 1; charTable[4][4] = -1; charTable[4][5] = -1; charTable[4][6] = 1;
+                charTable[5][0] = "E1"; charTable[5][1] = 2; charTable[5][2] = -1; charTable[5][3] = -1; charTable[5][4] = 2; charTable[5][5] = 0; charTable[5][6] = 0;
+                charTable[6][0] = "E2"; charTable[5][1] = 2; charTable[5][2] = -1; charTable[5][3] = -1; charTable[5][4] = 2; charTable[5][5] = 0; charTable[5][6] = 0;
+                break;
+
+            case "c2h":
+                break;
+
+            case "c3h":
+                break;
+
+            case "c4h":
+                break;
+
+            case "c5h":
+                break;
+
+            case "c6h":
+                break;
+
+            case "d2":
+                break;
+
+            case "d3":
+                break;
+
+            case "d4":
+                break;
+
+            case "d5":
+                break;
+
+            case "d6":
+                break;
+
+            case "d2v":
+                break;
+
+            case "d3v":
+                break;
+
+            case "d4v":
+                break;
+
+            case "d5v":
+                break;
+
+            case "d6v":
+                break;
+
+            case "d2h":
+                break;
+
+            case "d3h":
+                break;
+
+            case "d4h":
+                break;
+
+            case "d5h":
+                break;
+
+            case "d6h":
+                break;
+
+            case "td":
+                break;
+
+            case "o":
+                break;
+
+            case "oh":
+                break;
+
             default:
                 charTable = null;
                 break;
