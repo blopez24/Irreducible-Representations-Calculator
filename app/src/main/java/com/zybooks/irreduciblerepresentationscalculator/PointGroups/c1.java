5b746e872a375ac8ab646bcc6af1f4f72296573a
Hsiang-Li Chung <hchung14@ucsc.edu>
2019-11-10 03:38:35 -0800

    updated c1 layout

--- app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/PointGroups/c1.java
+++ app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/PointGroups/c1.java
@@ -2,12 +2,15 @@ package com.zybooks.irreduciblerepresentationscalculator.PointGroups;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.text.HtmlCompat;
 
 import com.zybooks.irreduciblerepresentationscalculator.MainActivity;
 import com.zybooks.irreduciblerepresentationscalculator.R;
@@ -15,35 +18,54 @@ import com.zybooks.irreduciblerepresentationscalculator.Results;
 
 public class c1 extends AppCompatActivity {
 
-    EditText value;
-    Button button;
-    TextView answerText, answer, step3;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.c1);
+        TextView c1_descr = findViewById(R.id.c1_descr);
+        c1_descr.setText(HtmlCompat.fromHtml(
+                "Enter the characters for the reducible representation of the C<sub>1</sub> point group below.", HtmlCompat.FROM_HTML_MODE_LEGACY));
     }
 
-
     public void result(View view) {
-        value = findViewById(R.id.c1_e_user_input);
-        button = findViewById(R.id.c1_submit);
-        answerText = findViewById(R.id.c1_text);
-        answer = findViewById(R.id.c1_answer);
-        step3 = findViewById(R.id.c1_step3);
+        EditText value1 = findViewById(R.id.c1_e_user_input);
+        Button button = findViewById(R.id.c1_submit);
+        Button reset = findViewById(R.id.c1_reset);
+        TextView answerText = findViewById(R.id.c1_text);
+        TextView answer = findViewById(R.id.c1_answer);
 
-        step3.setVisibility(View.GONE);
-        button.setVisibility(View.GONE);
-        answerText.setVisibility(View.VISIBLE);
-        answer.setVisibility(View.VISIBLE);
+        if (TextUtils.isEmpty(value1.getText().toString())) {
+            Toast.makeText(this,"Please enter the values for each cell!", Toast.LENGTH_LONG).show();
+        } else {
+            button.setVisibility(View.GONE);
+            answerText.setVisibility(View.VISIBLE);
+            answerText.setText(HtmlCompat.fromHtml("The reducible representation of C<sub>1</sub> reduces to: ", HtmlCompat.FROM_HTML_MODE_LEGACY));
+            answer.setVisibility(View.VISIBLE);
+            reset.setVisibility(View.VISIBLE);
 
-        String ans = value.getText().toString() + "A";
-        answer.setText(ans);
+            String ans = value1.getText().toString() + "A";
+            answer.setText(ans);
+        }
     }
 
     public void return_1(View view) {
         Intent it = new Intent(this, MainActivity.class);
         startActivity(it);
+    }
+
+    public void reset(View view) {
+        EditText value1 = findViewById(R.id.c1_e_user_input);
+        Button button = findViewById(R.id.c1_submit);
+        TextView answerText = findViewById(R.id.c1_text);
+        TextView answer = findViewById(R.id.c1_answer);
+        Button reset = findViewById(R.id.c1_reset);
+
+        value1.setText("");
+        button.setVisibility(View.VISIBLE);
+        answerText.setVisibility(View.GONE);
+        answer.setVisibility(View.GONE);
+        reset.animate();
+        reset.setVisibility(View.INVISIBLE);
+
     }
 }
