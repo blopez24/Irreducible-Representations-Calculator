6b06bcc568e7eae3ce91f240d7dd1e257499ac59
Hsiang-Li Chung <hchung14@ucsc.edu>
2019-11-09 17:00:02 -0800

    improved the layout for C2      button disappear and answer appears

--- app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/PointGroups/c2.java
+++ app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/PointGroups/c2.java
@@ -3,18 +3,22 @@ package com.zybooks.irreduciblerepresentationscalculator.PointGroups;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.zybooks.irreduciblerepresentationscalculator.MainActivity;
 import com.zybooks.irreduciblerepresentationscalculator.R;
-import com.zybooks.irreduciblerepresentationscalculator.Results;
 
 public class c2 extends AppCompatActivity {
 
     EditText value;
     EditText value2;
+    Button button;
+    TextView answerText, answer, step3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +30,23 @@ public class c2 extends AppCompatActivity {
     public void result(View view) {
         value = findViewById(R.id.c2_e_user_input);
         value2 = findViewById(R.id.c2_c2_user_input);
+        button = findViewById(R.id.submit);
+        answerText = findViewById(R.id.c2_text);
+        answer = findViewById(R.id.c2_answer);
+        step3 = findViewById(R.id.c2_step3);
 
-        Intent intent = new Intent(this, Results.class);
+        step3.setVisibility(View.GONE);
+        button.setVisibility(View.GONE);
+        answerText.setVisibility(View.VISIBLE);
+        answer.setVisibility(View.VISIBLE);
+        answer.setText("2A + 3B");
+
+        /*Intent intent = new Intent(this, Results.class);
         intent.putExtra("value", value.getText());
         intent.putExtra("results", value2.getText());
         startActivity(intent);
+
+         */
     }
 
     public void return_1(View view) {
