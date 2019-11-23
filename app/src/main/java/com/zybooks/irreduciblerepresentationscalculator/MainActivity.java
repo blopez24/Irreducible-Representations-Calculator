70d274d2943ce46188ca3d1b971c6aabc8b43046
Hsiang-Li Chung <hchung14@ucsc.edu>
2019-11-22 17:12:37 -0800

    table data file changed

--- app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/MainActivity.java
+++ app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/MainActivity.java
@@ -3,9 +3,11 @@ package com.zybooks.irreduciblerepresentationscalculator;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.text.HtmlCompat;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 
