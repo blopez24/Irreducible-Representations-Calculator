50c24906a26db7559ab5771ac3ea5065f734317c
jrodt333 <jrodt333@gmail.com>
2019-12-02 15:46:57 -0800

    made it so that the result string now has Cartesian 3N followed by vibration below it

--- app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/TableData.java
+++ app/src/main/java/com/zybooks/irreduciblerepresentationscalculator/TableData.java
@@ -71,23 +71,71 @@ public class TableData
             results[i] = (double)Math.round((results[i]) * 1000000) / 1000000;
         }
 
-
-        // we currently have total, have to subtract out rot and transl to get vib if Cartesian 3N contains all values
-        boolean subRotTransl = true;
-        for(int i = 0; i < results.length; i++)
-        {
-            if(results[i] == 0.0) {
-                subRotTransl = false;
-                break;
-            }
+        boolean reducible = true;
+        for(int i = 0; i < results.length; i++) {
+            if (results[i] != (int) results[i])
+                reducible = false;
         }
-        if(subRotTransl){
+
+
+        if( reducible )
+        {
+            int[] integerResults = new int[results.length];
+            for(int i = 0; i < results.length; i++)
+            {
+                integerResults[i] = (int)results[i];
+            }
+
+            // we currently have total, have to subtract out rot and transl to get vib if Cartesian 3N contains all values
             for(int i = 1; i < charTable.size(); i++)
             {
                 int rotCol = charTable.size();
                 int translCol = rotCol + 1;
                 results[i-1] = results[i-1] - Integer.parseInt(charTable.get(i).get(rotCol)) - Integer.parseInt(charTable.get(i).get(translCol));
             }
+
+            int[] integerResultsVib = new int[results.length];
+            for(int i = 0; i < results.length; i++)
+            {
+                integerResultsVib[i] = (int)results[i];
+            }
+
+            result = getResultsFormat(integerResults, "Cartesian 3N:\n") + getResultsFormat(integerResultsVib, "\nVibration:\n");
+
+        }
+        else
+        {
+            result = "The reducible representation cannot be reduced.";
+        }
+    }
+
+    public void calculate(double[] input)
+    {
+        // input is an array because the amount of args varies by element
+        int h = Integer.parseInt(charTable.get(0).get(0));
+
+        double[] results = new double[charTable.size()-1];
+        int rIndex = 0;
+
+        for(int row = 1; row < charTable.size(); row++)
+        {
+            int index = 0;
+            for(int col = 1; col < charTable.get(0).size(); col++)
+            {
+                results[rIndex] += Double.parseDouble(charTable.get(row).get(col)) * Double.parseDouble(charTable.get(0).get(col)) * input[index];
+                index++;
+            }
+            rIndex++;
+        }
+
+        for(int i = 0; i < results.length; i++)
+        {
+            results[i] /= h;
+            Log.i("table", "results " + results[i]);
+
+            // Rounds results to the sixth decimal place
+            // Necessary for sin, cos, decimal, and sqrt table values
+            results[i] = (double)Math.round((results[i]) * 1000000) / 1000000;
         }
 
         boolean reducible = true;
@@ -105,20 +153,33 @@ public class TableData
                 integerResults[i] = (int)results[i];
             }
 
-            result = getResultsFormat(integerResults);
+            // we currently have total, have to subtract out rot and transl to get vib if Cartesian 3N contains all values
+            for(int i = 1; i < charTable.size(); i++)
+            {
+                int rotCol = charTable.size();
+                int translCol = rotCol + 1;
+                results[i-1] = results[i-1] - Integer.parseInt(charTable.get(i).get(rotCol)) - Integer.parseInt(charTable.get(i).get(translCol));
+            }
+
+            int[] integerResultsVib = new int[results.length];
+            for(int i = 0; i < results.length; i++)
+            {
+                integerResultsVib[i] = (int)results[i];
+            }
+
+            result = getResultsFormat(integerResults, "Cartesian 3N:\n") + getResultsFormat(integerResultsVib, "\nVibration:\n");
+
         }
         else
         {
             result = "The reducible representation cannot be reduced.";
         }
-
-
-
     }
 
-    private String getResultsFormat(int[] array)
+    private String getResultsFormat(int[] array, String str)
     {
-        String formattedResults = "Cartesian 3N:\n";
+        //String formattedResults = "Cartesian 3N:\n";
+        String formattedResults = str;
         int nonZeroes = 0;
 
         for(int i = 0; i < array.length; i++)
